aacac81f7800e0b5d538e8b007760bf4f1110ebe
kmksmfvkmsfkmvkfsk <amenxia@gmail.com>
2017-11-07 19:23:27 -0800

    Animal Farm, Dice Roll , Rock Paper Siccors

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -17,10 +17,25 @@ AnimalFarm() {
 	/*
 	 * 1. Ask the user which animal they want, then play the sound of that
 	 * animal.
-	 */
-
+ */String dun =JOptionPane.showInputDialog("Which animal do yu wan't Cow,Duck,Dog,Cat,llama	");
+   if(dun.equals("Cow")){ 
+   playMoo()   ; 
+   }
+   if(dun.equals("Duck")){
+	   playQuack();
+   }
+   if(dun.equals("Dog")){
+   playWoof();
+   }
+   if(dun.equals("llama")){
+	   playllama();
+   }
+   if(dun.equals("Meow")){
+	   playCat();
+   }
+   }
 	/* 2. Make it so that the user can keep entering new animals. */
-}
+
 
 void playMoo() {
 	playNoise(mooFile);
@@ -33,7 +48,12 @@ void playQuack() {
 void playWoof() {
 	playNoise(woofFile);
 }
-
+void playllama(){
+	playNoise( llamaFile);
+}
+void playCat(){
+	playNoise(meowFile);
+}
 String quackFile = "quack.wav";
 String mooFile = "moo.wav";
 String woofFile = "woof.wav";
