5777b582de4f92000cf52bf993209b3705688d14
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2017-12-05 19:21:31 -0800

    that was easy string method 1 and 2

--- src/extra/StringMethods1.java
+++ src/extra/StringMethods1.java
@@ -2,20 +2,26 @@
 //    Level 0
 package extra;
 
+import javax.swing.JOptionPane;
 
 public class StringMethods1 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
+String hi="Your kind of cool";
 
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		System.out.println(hi.charAt(3));
 		// 3. Print the length of your String to the console.
-		//    HINT: .length
-
+	//    HINT: .length
+      System.out.println(hi.length());
 		// 4. Using a for loop, print one char at time to the console.
-		//    HINT: use .length() to determine how many loops
+		//    HINT: use .length() to determine how 
+	for (int i = 0; i < hi.length(); i++) { 
+		System.out.println(hi.charAt(i));
+		
+	}
 
 		
 		// 5. Pick a char inside your String, and use a loop to determine
@@ -23,7 +29,15 @@ public class StringMethods1 {
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
+	String ih =	
+	JOptionPane.showInputDialog("Pick a character that is the sentence");
+	char hi1 = ih.charAt(0);
+	for (int i = 0; i < hi.length(); i++) {
+		if(hi1==hi.charAt(i)) {
+			JOptionPane.showMessageDialog(null, ""+i);
+		}
 		
+	}
 	}
 }
 
